91b301853118243f224d27024fe0cecde2ad2587
Mirko1523 <134454433+Mirko1523@users.noreply.github.com>
2024-06-27 21:01:19 -0300

    ADD LOGO

--- Front-end/src/Components/Home/Home.jsx
+++ Front-end/src/Components/Home/Home.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import './home.styles.css'
-import video1 from '../../../MULTIMEDIA/Y2meta.app-Lexus Car Dealership –  FPV Drone Fly Through-(1080p).mp4'
+import video1 from '../../../MULTIMEDIA/VIDEOFONDOHOME.mp4'
+
 function HomePage() {
   return(
     <div>
+      <div className="video-Home">
          <video src={video1} autoPlay loop muted playsInline className="video-background"
-         style={{
-          width: '225vh', 
-          height: '75vh', 
-          objectFit: 'cover', 
-          border:'50px',
-        }}
          ></video>
-         
+      </div>   
    
     </div>
   )
--- Front-end/src/Components/navBar/navBar.jsx
+++ Front-end/src/Components/navBar/navBar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navBar.styles.css";
+import img1 from '../../../MULTIMEDIA/FotoFondoHome.png';
 
 function Nav() {
-   const handleGoToHome = () => {
+  const handleGoToHome = () => {
     window.location.href = "/";
   };
+
   return (
     <div className="allnav-nav">
       <div className="title-nav">
-        <NavLink onClick={handleGoToHome} className="navLink-title"><h1>Milles</h1></NavLink>
+        <NavLink onClick={handleGoToHome} className="navLink-title">
+          <img src={img1} className="nav-logo" alt="Logo" />
+        </NavLink>
       </div>
       <div className="navlink-nav">
         <NavLink to="/vehiculos" className="navlink-button">
-          Vehiculos
+          Vehículos
         </NavLink>
-        <NavLink to="/contactanos" className="navlink-button">
+        <NavLink to="/nosotros" className="navlink-button">
           Nosotros
         </NavLink>
-        <NavLink to="/b" className="navlink-button">
+        <NavLink to="/contactanos" className="navlink-button">
           Contacto
         </NavLink>
       </div>
